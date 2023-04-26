3e5aaa7b6681acb398502b52fe043b398b0a436f
Martin Rosas <mlrosas21@hotmail.com>
2023-04-25 21:44:41 -0300

    sorting using useMemo

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
 import { searchMovies } from "../services/movies";
 
-export function useMovies({ search }) {
+export function useMovies({ search, sort }) {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
+  // Evitar búsqueda duplicada
+  const prevSearch = useRef(search);
 
   const getMovies = async () => {
-    try{
-      setLoading(true)
-      setError(null)
-      const movies = await searchMovies({search})
-      setMovies(movies)
+    // Evitar búsqueda duplicada
+    if(search === prevSearch.current) return
+
+    try {
+      setLoading(true);
+      setError(null);
+      prevSearch.current = search
+      const movies = await searchMovies({ search });
+      setMovies(movies);
     } catch (err) {
-      setError(err)
+      setError(err);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
-  
-  return { getMovies, movies, error, loading };
+
+  const sortedMovies = useMemo(() => {
+    console.log(movies);
+    return sort ? [...movies].sort((a,b) => a.title.localeCompare(b.title)) : movies
+  }, [sort, movies])
+
+  return { getMovies, movies: sortedMovies , error, loading };
 }
