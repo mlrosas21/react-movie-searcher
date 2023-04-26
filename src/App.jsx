61978bb67ee6512a23fa06c3f0349ef297da648a
Martin Rosas <mlrosas21@hotmail.com>
2023-04-25 21:45:10 -0300

    sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import MovieList from "./components/MovieList";
 import { useSearch } from "./hooks/useSearch";
 import { useMovies } from "./hooks/useMovies";
+import { Movies } from "./components/Movies";
+import { useState } from "react";
 
 function App() {
+  const [sort, setSort] = useState(false);
   const { search, setSearch, error } = useSearch();
-  const { getMovies, movies, loading: moviesLoading } = useMovies({search});
-  // const inputRef = useRef(); // <- no tan recomendado, porque se ensucia al tratar varios inputs 
+  const { getMovies, movies, loading: moviesLoading } = useMovies({ search, sort });
+  // const inputRef = useRef(); // <- no tan recomendado, porque se ensucia al tratar varios inputs
 
   const handleChange = (event) => {
     setSearch(event.target.value);
@@ -34,23 +36,38 @@ function App() {
       <header>
         <form onSubmit={handleSubmit}>
           <h1>Buscador de peliculas</h1>
-          <input
-            name="query"
-            value={search}
-            onChange={handleChange}
-            placeholder="Avatar, Harry Potter, etc..."
-            // ref={inputRef}
-          />
-          <button type="submit" style={{ marginLeft: "1rem" }}>
-            Buscar
-          </button>
+          <span>
+            <input
+              name="query"
+              value={search}
+              onChange={handleChange}
+              placeholder="Avatar, Harry Potter, etc..."
+              // ref={inputRef}
+            />
+            <button type="submit" style={{ marginLeft: "1rem" }}>
+              Buscar
+            </button>
+          </span>
+          <span>
+            <label>Orden alfabético: </label>
+            <input
+              type="checkbox"
+              name="sort"
+              onChange={() => setSort((prevState) => !prevState)}
+              checked={sort}
+            />
+          </span>
         </form>
       </header>
 
       <main>
         {error && <h4>{error}</h4>}
 
-        {moviesLoading ? <p>Cargando películas...</p> : <MovieList movies={movies} />}
+        {moviesLoading ? (
+          <p>Cargando películas...</p>
+        ) : (
+          <Movies movies={movies} />
+        )}
       </main>
     </>
   );
